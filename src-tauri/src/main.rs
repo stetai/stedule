cd19b957e7a5c8c0e987d0dcc52d04b39455cb5d
stetai <taing@web.de>
2026-05-05 00:46:47 +0200

    feat: add filesystem plugin

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    stedule_lib::run()
+    tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
