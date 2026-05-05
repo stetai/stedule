d2d8986a1a59c64c06ec2280cb725e5cc724f8f4
stetai <taing@web.de>
2026-05-05 23:35:57 +0200

    fix: nvm the issue is somewhere else

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,39 +3,6 @@
 use tauri::command;
 use tauri_plugin_dialog::DialogExt;
 
-/*#[command]
-async fn open_calendar(app: tauri::AppHandle) -> Result<Option<serde_json::Value>, String> {
-    let (tx, rx) = tokio::sync::oneshot::channel();
-
-    app.dialog().file()
-        //.add_filter("Calendar", &["ics"]) //automatically returns null
-        .pick_file(move |maybe_path| {
-            // Harmless: receiver dropped.
-            println!("#####dialog returned: {:?}", maybe_path);
-            let _ = tx.send(maybe_path);
-        });
-
-    let maybe_path = rx.await.map_err(|e| e.to_string())?;
-
-    let file_path = match maybe_path {
-        None => return Ok(None), //user cancelled
-        Some(p) => p,
-    };
-
-    let path_str = file_path.to_string();
-
-    let name = path_str
-        .split('/')
-        .last()
-        .unwrap_or("calendar.ics")
-        .to_string();
-
-    Ok(Some(serde_json::json!({
-        "path": path_str,
-        "name": name
-    })))
-}*/
-
 #[command]
 async fn open_calendar(app: tauri::AppHandle) -> Result<Option<String>, String> {
     let (tx, rx) = tokio::sync::oneshot::channel();
